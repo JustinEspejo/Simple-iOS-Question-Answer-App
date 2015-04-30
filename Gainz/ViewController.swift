f0ae64bba9dc88659b3333d91a2aabca3b6cddfe
Deniz Tolga Turgut <deniz@amicuscreative.com>
2015-04-30 17:59:16 -0400

    Initial Commit

---
+++ Gainz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gainz
+//
+//  Created by Deniz Tolga Turgut on 4/30/15.
+//  Copyright (c) 2015 Deniz Tolga Turgut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
